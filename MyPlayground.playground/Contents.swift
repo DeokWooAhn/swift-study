a89b2de92d428f2711d368443a116f1259447593
안덕우 <fjrlvkdlxj@naver.com>
2024-05-16 22:00:44 +0900

    2024.05.16

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -22,37 +22,61 @@ import UIKit
 //
 //abc(function, 1, 2)
 
-var a: Int? = 1
+//var a: Int? = 1
+//
+//if a != nil {
+//    print("aa")
+//} else {
+//    print("bb")
+//}
+//
+//if let unwrappedA = a {
+//    print("aa")
+//} else {
+//    print("bb")
+//}
+//
+//var food: String? = "rice"
+//
+//if let favoriteFood = food {
+//    print(favoriteFood)
+//} else {
+//    print("nil")
+//}
+//
+//func printNickName(_ name: String?) {
+//    guard let nickName = name else {
+//        print("nil")
+//        return
+//    }
+//    
+//    print(nickName)
+//}
+//
+//printNickName("Tom")
 
-if a != nil {
-    print("aa")
-} else {
-    print("bb")
+//var info: [String: String] = ["name": "Ahn", "job": "developer", "city": "seoul"]
+//info["city"] = "busan"
+//
+//func printDictionary(dic: [String: String]) {
+//    if let name = dic["name"], let job = dic["job"], let city = dic["city"] {
+//        print(name, job, city)
+//    } else {
+//        print("is nil")
+//    }
+//}
+//
+//printDictionary(dic: info)
+
+var multiplyClosure: (Int, Int) -> Int = { a, b in
+    return a * b
 }
 
-if let unwrappedA = a {
-    print("aa")
-} else {
-    print("bb")
+let result = multiplyClosure(4, 2)
+
+func operateNum(a: Int, b: Int, operation: (Int, Int) -> Int) -> Int {
+    let result = operation(a, b)
+    return result
 }
 
-var food: String? = "rice"
-
-if let favoriteFood = food {
-    print(favoriteFood)
-} else {
-    print("nil")
-}
-
-func printNickName(_ name: String?) {
-    guard let nickName = name else {
-        print("nil")
-        return
-    }
-    
-    print(nickName)
-}
-
-printNickName("Tom")
-
-
+operateNum(a: 4, b: 2, operation: multiplyClosure)
