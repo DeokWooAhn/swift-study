9d7602cd8f6e4a9761e4c8ea08b8a66f05b00f9b
안덕우 <fjrlvkdlxj@naver.com>
2024-06-13 00:31:22 +0900

    2024.06.13

---
+++ SybolRoller/SybolRoller/SymbolRollerViewController.swift
@@ -0,0 +1,48 @@
+//
+//  SymbolRollerViewController.swift
+//  SybolRoller
+//
+//  Created by 안덕우 on 6/13/24.
+//
+
+import UIKit
+
+class SymbolRollerViewController: UIViewController {
+
+    let symblos: [String] = ["sun.min", "moon", "cloud", "wind", "snowflake"]
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var button: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imageView.image = UIImage(systemName: "cloud")
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    
+    
+    @IBAction func buttonTapped(_ sender: Any) {
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
